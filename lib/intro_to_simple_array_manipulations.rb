04caf27fbe75d6a289a33d1c7622df3b58a94627
Owen Abbott <ocabbott@gmail.com>
2020-01-27 02:19:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,3 +15,33 @@ end
 def pop_with_args(array)
   array.pop(2)
 end
+
+def using_shift(array)
+  first_element=array[0]
+  array.shift()
+  return first_element
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def concat(array, element)
+  array[4].insert(element)
+end
+
+def using_uniq(array)
+  array.uniq()
+end
+
+def using_flatten(array)
+  array.flatten()
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete(integer)
+end
